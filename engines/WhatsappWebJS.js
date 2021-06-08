c560eb207a09d5d2679df857dc2cbdefaf44492d
Eduardo Policarpo <30879448+edupoli@users.noreply.github.com>
2021-06-08 08:17:13 -0300

    retirado chrome launcher do Whatsapp-webJS

--- engines/WhatsappWebJS.js
+++ engines/WhatsappWebJS.js
@@ -27,7 +27,6 @@ module.exports = class WhatsappWebJS {
             client = new Client({
                 restartOnAuthFail: true,
                 puppeteer: {
-                    executablePath: chromeLauncher || null,
                     headless: true,
                     args: [
                         '--no-sandbox',
@@ -153,101 +152,3 @@ module.exports = class WhatsappWebJS {
         );
     }
 }
-
-// const { Client } = require('whatsapp-web.js');
-// const qrcode = require('qrcode-terminal');
-// const qrcodeBase64 = require('qrcode');
-// const { Launcher } = require('chrome-launcher');
-// let chromeLauncher = Launcher.getInstallations()[0];
-// const Sessions = require('../controllers/sessions');
-// const events = require('../controllers/events-whatswebjs');
-// const webhooks = require('../controllers/webhooks');
-// const fs = require('fs');
-// const path = require('path');
-// const superagent = require('superagent');
-// require('superagent-queue');
-
-// module.exports = class WhatsappWebJS {
-
-//     static createSession = async function (session) {
-//         return new Promise((resolve, rejects) => {
-//             console.log('Creating session: ' + session);
-//             const SESSION_FILE_PATH = path.resolve(`./tokens/${session}.json`);
-
-//             let sessionCfg;
-//             if (fs.existsSync(SESSION_FILE_PATH)) {
-//                 sessionCfg = require(SESSION_FILE_PATH);
-//             }
-
-//             const client = new Client({
-//                 restartOnAuthFail: true,
-//                 puppeteer: {
-//                     headless: true,
-//                     executablePath: chromeLauncher || null,
-//                     args: [
-//                         '--no-sandbox',
-//                         '--disable-setuid-sandbox',
-//                         '--disable-dev-shm-usage',
-//                         '--disable-accelerated-2d-canvas',
-//                         '--no-first-run',
-//                         '--no-zygote',
-//                         '--single-process',
-//                         '--disable-gpu'
-//                     ],
-//                 },
-//                 session: sessionCfg
-//             });
-
-//             client.initialize();
-
-//             client.on('qr', (qr) => {
-//                 console.log('QR RECEIVED', qr);
-//                 qrcode.generate(qr, { small: true });
-//                 qrcodeBase64.toDataURL(qr, (err, url) => {
-//                     webhooks.wh_qrcode(session, url)
-//                 });
-//             });
-
-//             client.on('ready', () => {
-//                 console.log('READY... WhatsApp is ready');
-//                 resolve(client)
-//             });
-
-//             client.on('authenticated', (session) => {
-//                 console.log('Whatsapp is authenticated!');
-//                 sessionCfg = session;
-//                 fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
-//                     if (err) {
-//                         console.error(err);
-//                     }
-//                 });
-//                 Sessions.addInfoSession(session, {
-//                     wa_browser_id: sessao.WABrowserId,
-//                     wa_secret_bundle: sessao.WASecretBundle,
-//                     wa_token_1: sessao.WAToken1,
-//                     wa_token_2: sessao.WAToken2
-//                 })
-//             });
-
-//             client.on('change_state', (reason) => {
-//                 console.log('Client was change state', reason);
-//                 webhooks.wh_status(session, reason)
-//             });
-
-//             client.on('auth_failure', function (session) {
-//                 console.log('Auth failure, restarting...');
-//             });
-
-//             client.on('disconnected', (reason) => {
-//                 console.log('Whatsapp is disconnected!');
-//                 fs.unlinkSync(SESSION_FILE_PATH, function (err) {
-//                     if (err) return console.log(err);
-//                     console.log('Session file deleted!');
-//                 });
-//                 client.destroy();
-//                 client.initialize();
-//             });
-//         })
-//     }
-// }
-
